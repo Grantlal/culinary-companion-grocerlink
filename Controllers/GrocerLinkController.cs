907a74ceb24637334486cdef0ce4109596ad078e
Grant <granttlalonde@gmail.com>
2020-02-24 20:52:04 -0600

    Added Grocer Link

---
+++ Controllers/GrocerLinkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GrocerLink.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GrocerLinkController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            try
+            {
+                return "Successful connect to Grocer Link";
+            }
+            catch
+            {
+                return "No";
+            }
+        }
+    }
+}
